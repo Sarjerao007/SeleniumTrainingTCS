7eaf663f76011786c2c3eeadb35362fe6cf30bcd
Himmatrao Bochare <sachinbochare94@gmail.com>
2023-08-09 07:45:06 +0000

    modify Login Script

--- src/test/java/testScripts/LoginTest.java
+++ src/test/java/testScripts/LoginTest.java
@@ -12,8 +12,17 @@ public class LoginTest {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://the-internet.herokuapp.com/login");
+		
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("tomsmith");
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("SuperSecretPassword!");
+		driver.findElement(By.xpath("//button[@class='radius']")).click();
+		
+		
+		//driver.findElement(By.id("username")).sendKeys("tomsmith");
+		//driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
+		//driver.findElement(By.className("radius")).click();
 		//driver.findElement(By.linkText("Elemental Selenium")).click();
-		driver.findElement(By.partialLinkText("Elemental")).click();
+		//driver.findElement(By.partialLinkText("Elemental")).click();
 		
 		
 	}
